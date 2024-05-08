8a85abcf37c7c4f2bd40d211229de6b7a34a6438
Christian Hill <xn.hill@gmail.com>
2024-05-08 10:48:21 +0200

    Refactor search and filter pages

--- exomolhr/calculate/urls.py
+++ exomolhr/calculate/urls.py
@@ -5,17 +5,6 @@ from django.urls import re_path, include
 from . import views
 
 urlpatterns = [
-    path('', views.molecules, name='molecules'),
-    re_path(r'^(?P<molecules_url>[\w_]+)/(?P<isotopologues_url>[\w\d\0-9]+)/(?P<datasets_url>[\w_]+)/$', views.dofilters, name='dofilters'),
-    re_path(r'^(?P<molecules_url>[\w_]+)/(?P<isotopologues_url>[\w\d\0-9]+)/$', views.datasets, name='datasets'),
-    re_path(r'^(?P<molecules_url>[\w_]+)/$', views.isotopologues, name='isotopologues'),
-    
-    
-    
-    # path('<str:molecule>/<str:isotopologue>/<str:dataset>', views.filters, name='filters'),
-    # path('download/<str:molecule>/<str:isotopologue>/<str:dataset>/', views.download_localfile, name='localcsv'),
-
-    # path('search/', views.search, name='search'),
-    # path('results/', views.results, name='results'),
-    # path('download/', views.download, name='search_results_download'),
+    path('', views.get_calculate_params, name='get_calculate_params'),
+    path('get-data/', views.get_data, name='get_data'),
 ]
