f84730aebc7d7ea6a72d0cc97dd262d2c444cd7e
Jingxin Zhang <jingxin.zhang.19@ucl.ac.uk>
2026-03-13 08:47:31 +0000

    Add API for master file and CSV files

--- exomolhr/linelist/urls.py
+++ exomolhr/linelist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.views.generic import RedirectView
 from . import views
 
 app_name = "linelist"
@@ -10,8 +10,10 @@ urlpatterns = [
     path("citation/", views.citation, name="citation"),
     path("updates/", views.updates, name="updates"),
     path("db/", views.get_linelist, name="get_linelist"),
+    path("db/<str:csv_filename>/", views.download_csv, name="download_csv"),
     path("pf/<str:pf_filename>/", views.view_pf, name="view_pf"),
     path("get-data/", views.get_data, name="get_data"),
     path("get-data/ajax-data", views.ajax_data, name="ajax_data"),
     path("get-data/download/", views.download_archive, name="download_archive"),
+    path("exomolhr.all.json", RedirectView.as_view(url='/exomolhr/db/exomolhr.all.json')),
 ]
